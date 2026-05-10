240a8ca2f41f338d82101d529f83a54b0ca54077
Surya <suryadevaroy@gmail.com>
2026-05-11 01:00:41 +0530

    Changed braces

--- TEMP/Evenodd.sh
+++ TEMP/Evenodd.sh
@@ -1,7 +1,7 @@
 for i in {1..10}; do
-if [ $ (expr i % 2) -eq 0 ]; then
+  if [ $(expr $i % 2) -eq 0 ]; then
     echo "$i is Even"
   else
-   echo "$i is odd"
-fi
+    echo "$i is Odd"
+  fi
 done
